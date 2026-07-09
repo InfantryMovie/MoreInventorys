9dd85ac8c8a001388772c73eb02421a9a4d48c90
InfantryMovie <infantryman.damir@yandex.ru>
2026-07-09 10:05:06 +0300

    добавил хелп для полки

--- MoreInventorys/src/BlockFolder/FirstShelfBlock.cs
+++ MoreInventorys/src/BlockFolder/FirstShelfBlock.cs
@@ -1,23 +1,130 @@
-﻿using System;
+﻿using MoreInventorys.src.BlockEntityFolder;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MoreInventorys.src.BlockEntityFolder;
+using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
 using Vintagestory.API.MathTools;
+using Vintagestory.API.Util;
+using Vintagestory.GameContent;
 
 namespace MoreInventorys.src.BlockFolder
 {
     public class FirstShelfBlock : Block
     {
+        WorldInteraction[]? interactions;
         public override void OnLoaded(ICoreAPI api)
         {
-
             base.OnLoaded(api);
-            // Todo: Add interaction help
+            PlacedPriorityInteract = true;
 
+            interactions = ObjectCacheUtil.GetOrCreate(api, "firstshelfInteractions", () =>
+            {
+                List<ItemStack> usableItemStacklist = new List<ItemStack>();
+                List<ItemStack> shelvableStacklist = new List<ItemStack>();
+
+                foreach (var obj in api.World.Collectibles)
+                {
+                    if (obj?.Attributes?["mealContainer"]?.AsBool() == true ||
+                        obj is IContainedInteractable ||
+                        obj is IBlockMealContainer ||
+                        obj?.Attributes?["canSealCrock"]?.AsBool() == true)
+                    {
+                        usableItemStacklist.Add(new ItemStack(obj));
+                    }
+
+                    if (BEFirstShelf.GetShelvableLayout(new ItemStack(obj)) != null)
+                    {
+                        if (obj is BlockPie pieBlock)
+                        {
+                            var stack = new ItemStack(obj);
+                            stack.Attributes.SetInt("pieSize", 4);
+                            stack.Attributes.SetString("topCrustType", "square");
+                            stack.Attributes.SetInt("bakeLevel", pieBlock.Variant["state"] switch { "raw" => 0, "partbaked" => 1, "perfect" => 2, "charred" => 3, _ => 0 });
+                            ItemStack doughStack = new(api.World.GetItem("dough-spelt"), 2);
+                            ItemStack fillingStack = new(api.World.GetItem("fruit-redapple"), 2);
+                            pieBlock.SetContents(stack, [doughStack, fillingStack, fillingStack, fillingStack, fillingStack, doughStack]);
+                            stack.Attributes.SetFloat("quantityServings", 1);
+                            shelvableStacklist.Add(stack);
+                        }
+                        else shelvableStacklist.Add(new ItemStack(obj));
+                    }
+                }
+
+                var sstacks = shelvableStacklist.ToArray();
+
+                return new WorldInteraction[]
+                {
+                    new WorldInteraction()
+                    {
+                        ActionLangCode = "Положить на полку",
+                        MouseButton = EnumMouseButton.Right,
+                        Itemstacks = sstacks,
+                        GetMatchingStacks = (wi, bs, es) =>
+                        {
+                            var beshelf = api.World.BlockAccessor.GetBlockEntity(bs.Position) as BEFirstShelf;
+                            if (beshelf == null) return null;
+
+                            if (usableItemStacklist.All(stack => !beshelf.CanUse(stack, bs)))
+                            {
+                                var result = usableItemStacklist.Where(stack => beshelf.CanPlace(stack, bs, out bool canTake)).ToArray();
+                                return result.Length > 0 ? result : null;
+                            }
+                            return null;
+                        }
+                    },
+                    new WorldInteraction()
+                    {
+                        ActionLangCode = "Положить на полку",
+                        HotKeyCode = "shift",
+                        MouseButton = EnumMouseButton.Right,
+                        Itemstacks = sstacks,
+                        GetMatchingStacks = (wi, bs, es) =>
+                        {
+                            var beshelf = api.World.BlockAccessor.GetBlockEntity(bs.Position) as BEFirstShelf;
+                            if (beshelf == null) return null;
+
+                            if (usableItemStacklist.Any(stack => beshelf.CanUse(stack, bs)))
+                            {
+                                var result = usableItemStacklist.Where(stack => beshelf.CanPlace(stack, bs, out bool canTake)).ToArray();
+                                return result.Length > 0 ? result : null;
+                            }
+                            return null;
+                        }
+                    },
+                    new WorldInteraction()
+                    {
+                        ActionLangCode = "Забрать с полки",
+                        MouseButton = EnumMouseButton.Right,
+                        RequireFreeHand = true,
+                        ShouldApply = (wi, bs, es) =>
+                        {
+                            var beshelf = api.World.BlockAccessor.GetBlockEntity(bs.Position) as BEFirstShelf;
+                            if (beshelf == null) return false;
+
+                            beshelf.CanPlace(null, bs, out bool canTake);
+                            return canTake;
+                        }
+                    }
+                };
+            });
+        }
+
+        public override WorldInteraction[] GetPlacedBlockInteractionHelp(IWorldAccessor world, BlockSelection selection, IPlayer forPlayer)
+        {
+            var worldInteractions = base.GetPlacedBlockInteractionHelp(world, selection, forPlayer);
+
+            // Проверяем права доступа
+            var resp = world.Claims.TestAccess(forPlayer, selection.Position, EnumBlockAccessFlags.Use);
+            if (resp == EnumWorldAccessResponse.Granted)
+            {
+                return worldInteractions?.Concat(interactions!).ToArray() ?? interactions!;
+            }
+
+            return worldInteractions ?? new WorldInteraction[0];
         }
 
         public override bool DoPartialSelection(IWorldAccessor world, BlockPos pos)
